194788b5db91b4d1866731b9ea9d2f70bc05635f
leyesmario <57366610+leyesmario@users.noreply.github.com>
2019-12-17 20:21:21 -0500

    Create Codigo.mk

---
+++ Codigo.mk
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status doc
+sudo apt install docker-ce
+sudo systemctl status docker
+
+##De esta manera se instala docker en ubuntu-linux
